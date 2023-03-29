464a60804c1687d66cde128b49b3b9462262e665
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-03-29 12:48:41 +0200

    Add activities package

--- app/src/main/java/com/example/fitnesstracker/Activities/MainActivity.java
+++ app/src/main/java/com/example/fitnesstracker/Activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.fitnesstracker;
+package com.example.fitnesstracker.Activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.fitnesstracker.R;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
--- app/src/main/java/com/example/fitnesstracker/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/fitnesstracker/Activities/RegisterActivity.java
@@ -1,4 +1,4 @@
-package com.example.fitnesstracker;
+package com.example.fitnesstracker.Activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +13,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.fitnesstracker.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
