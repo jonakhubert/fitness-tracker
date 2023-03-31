a0d4f6b7e22037e7f8e29afd68e13d283d8da0f3
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-03-31 21:33:07 +0200

    Remove email verification and logout button

--- app/src/main/java/com/example/fitnesstracker/Activities/MainActivity.java
+++ app/src/main/java/com/example/fitnesstracker/Activities/MainActivity.java
@@ -1,74 +1,27 @@
 package com.example.fitnesstracker.Activities;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.fitnesstracker.R;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
-    private TextView verifyMessage;
-    private Button verifyButton;
     private BottomNavigationView bottomNavigationView;
-    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        verifyMessage = findViewById(R.id.verifyMessage);
-        verifyButton = findViewById(R.id.verifyButton);
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavController navController = Navigation.findNavController(findViewById(R.id.fragment));
 
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
-
-        firebaseAuth = FirebaseAuth.getInstance();
-
-        FirebaseUser user = firebaseAuth.getCurrentUser();
-
-        if(!user.isEmailVerified())
-        {
-            verifyMessage.setVisibility(View.VISIBLE);
-            verifyButton.setVisibility(View.VISIBLE);
-
-            verifyButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void unused) {
-                            Toast.makeText(v.getContext(), "Verification email has been sent.", Toast.LENGTH_SHORT).show();
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(v.getContext(), "Verification email has not been sent. " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
-            });
-        }
-    }
-
-    public void logout(View view) {
-        FirebaseAuth.getInstance().signOut();
-        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-        finish();
     }
 }
