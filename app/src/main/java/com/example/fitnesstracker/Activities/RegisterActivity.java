98dd09484d30955a1341528abd2078101f5c2fc8
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-03-31 16:37:51 +0200

    Add users collection

--- app/src/main/java/com/example/fitnesstracker/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/fitnesstracker/Activities/RegisterActivity.java
@@ -1,11 +1,11 @@
 package com.example.fitnesstracker.Activities;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,13 +14,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.fitnesstracker.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText name, email, password;
@@ -28,6 +28,7 @@ public class RegisterActivity extends AppCompatActivity {
     private TextView loginRef;
     private ProgressBar progressBar;
     private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +38,13 @@ public class RegisterActivity extends AppCompatActivity {
         name = findViewById(R.id.name);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
+
         registerButton = findViewById(R.id.registerButton);
         loginRef = findViewById(R.id.loginRef);
         progressBar = findViewById(R.id.registerProgressBar);
 
         firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
 
         if(firebaseAuth.getCurrentUser() != null)
         {
@@ -77,32 +80,26 @@ public class RegisterActivity extends AppCompatActivity {
 
                 // user registration
 
-                firebaseAuth.createUserWithEmailAndPassword(emailStr, passwordStr).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful())
-                        {
-                            FirebaseUser user = firebaseAuth.getCurrentUser();
-                            user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
-                                @Override
-                                public void onSuccess(Void unused) {
-                                    Toast.makeText(RegisterActivity.this, "Verification email has been sent.", Toast.LENGTH_SHORT).show();
-                                }
-                            }).addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Toast.makeText(RegisterActivity.this, "Verification email has not been sent. + " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                                }
-                            });
+                firebaseAuth.createUserWithEmailAndPassword(emailStr, passwordStr).addOnCompleteListener(task -> {
+                    if(task.isSuccessful())
+                    {
+                        Toast.makeText(RegisterActivity.this, "User created successfully!", Toast.LENGTH_SHORT).show();
+                        FirebaseUser user = firebaseAuth.getCurrentUser();
+                        user.sendEmailVerification().addOnSuccessListener(unused -> Toast.makeText(RegisterActivity.this, "Verification email has been sent.", Toast.LENGTH_SHORT).show())
+                                .addOnFailureListener(e -> Toast.makeText(RegisterActivity.this, "Verification email has not been sent. + " + e.getMessage(), Toast.LENGTH_SHORT).show());
 
-                            Toast.makeText(RegisterActivity.this, "User created successfully!", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        }
-                        else
-                        {
-                            Toast.makeText(RegisterActivity.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                            progressBar.setVisibility(View.GONE);
-                        }
+                        DocumentReference documentReference = firebaseFirestore.collection("users").document(user.getUid());
+                        Map<String, Object> fUser = new HashMap<>();
+                        fUser.put("name", name.getText().toString());
+                        fUser.put("email", email.getText().toString());
+
+                        documentReference.set(fUser);
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    }
+                    else
+                    {
+                        Toast.makeText(RegisterActivity.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.GONE);
                     }
                 });
             }
