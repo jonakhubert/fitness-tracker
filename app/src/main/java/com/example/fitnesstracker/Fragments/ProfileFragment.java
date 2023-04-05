9a750e012e48da660aba717b50e50406e319c0eb
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-04-05 22:33:55 +0200

    Fetch user data from firestore

--- app/src/main/java/com/example/fitnesstracker/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/fitnesstracker/Fragments/ProfileFragment.java
@@ -7,12 +7,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.fitnesstracker.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class ProfileFragment extends Fragment {
     private View view;
+    private TextView userName, userEmail;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
+    private String userId;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -21,6 +29,20 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        userName = view.findViewById(R.id.userName);
+        userEmail = view.findViewById(R.id.userEmail);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        userId = firebaseAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = firebaseFirestore.collection("users").document(userId);
+        documentReference.addSnapshotListener(getActivity(), (value, error) -> {
+           userName.setText(value.getString("name"));
+           userEmail.setText(value.getString("email"));
+        });
 
         return view;
     }
