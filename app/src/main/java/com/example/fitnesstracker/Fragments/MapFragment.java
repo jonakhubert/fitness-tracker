e6834880c998e7f172dc7effd7ad7cfe097977ad
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-04-19 22:45:22 +0200

    Add map fragment to map activity

---
+++ app/src/main/java/com/example/fitnesstracker/Fragments/MapFragment.java
@@ -0,0 +1,42 @@
+package com.example.fitnesstracker.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fitnesstracker.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Objects;
+
+public class MapFragment extends Fragment {
+    private View view;
+
+    public MapFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        Objects.requireNonNull(supportMapFragment).getMapAsync(googleMap -> googleMap.setOnMapClickListener(latLng -> {
+            googleMap.getUiSettings().setZoomControlsEnabled(true);
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(latLng);
+            markerOptions.title(latLng.latitude + " " + latLng.longitude);
+            googleMap.clear();
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+            googleMap.addMarker(markerOptions);
+        }));
+
+        return view;
+    }
+}
