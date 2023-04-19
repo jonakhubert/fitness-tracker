309eeffd93a1bfee2f669d1900dc6976274391e0
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-04-19 22:42:46 +0200

    Add map activity and layout

---
+++ app/src/main/java/com/example/fitnesstracker/Activities/MapActivity.java
@@ -0,0 +1,22 @@
+package com.example.fitnesstracker.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+import com.example.fitnesstracker.Fragments.MapFragment;
+import com.example.fitnesstracker.R;
+
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        Fragment fragment = new MapFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragment).commit();
+    }
+}
