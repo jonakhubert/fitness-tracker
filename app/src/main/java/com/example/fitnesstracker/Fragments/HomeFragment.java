763296cb989931ae8decbf6f592a98c9c749cc7d
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-04-19 17:15:34 +0200

    Add google advertisement to home fragment

--- app/src/main/java/com/example/fitnesstracker/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/fitnesstracker/Fragments/HomeFragment.java
@@ -9,10 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.fitnesstracker.R;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 
 public class HomeFragment extends Fragment {
     private View view;
+    private AdView ad;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -21,6 +25,12 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_home, container, false);
+        ad = view.findViewById(R.id.adView);
+
+        MobileAds.initialize(requireActivity(), initializationStatus -> {});
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+        ad.loadAd(adRequest);
 
         return view;
     }
