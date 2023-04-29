a14d9b5ebed902b09882f55ca372bc02d56668ce
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-04-29 21:06:22 +0200

    Create map activity after app is launched

--- app/src/main/java/com/example/fitnesstracker/Activities/MainActivity.java
+++ app/src/main/java/com/example/fitnesstracker/Activities/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.fitnesstracker.R;
@@ -18,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent mapIntent = new Intent(this, MapActivity.class);
+        mapIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP); // Add this flag to prevent starting a new instance of MapActivity if it is already running
+        startActivity(mapIntent);
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavController navController = Navigation.findNavController(findViewById(R.id.fragment));
