9813acfccb3f61958aef036d66988017fe746ab6
jonakhubert <75631040+jonakhubert@users.noreply.github.com>
2023-04-30 12:54:37 +0200

    Remove redundant creation of MapActivity

--- app/src/main/java/com/example/fitnesstracker/Activities/MainActivity.java
+++ app/src/main/java/com/example/fitnesstracker/Activities/MainActivity.java
@@ -20,10 +20,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent mapIntent = new Intent(this, MapActivity.class);
-        mapIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP); // Add this flag to prevent starting a new instance of MapActivity if it is already running
-        startActivity(mapIntent);
-
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavController navController = Navigation.findNavController(findViewById(R.id.fragment));
 
